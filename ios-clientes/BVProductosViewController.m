4d1ba175949eaa07debba39cc4d6a79170e1b1b1
Pedro Cortez <pedro.cortez@bicevida.cl>
2013-07-02 11:54:39 -0400

    Se agrego la view de perfil - Fixes #5 Fixes #56

--- ios-clientes/BVProductosViewController.m
+++ ios-clientes/BVProductosViewController.m
@@ -70,7 +70,7 @@
 {
     GradientBackgroundHeader *header = [[GradientBackgroundHeader alloc]initWithDelegate:self labelHeight:30.0];
     [header setLeftLabelText:[[[[self.fetchedResultsController sections]objectAtIndex:section] name] capitalizedString] isFontSizeBig:NO];
-    header.haveTopBorder = section==0;
+    header.haveTopBorder = NO;
     return header;
 }
 
